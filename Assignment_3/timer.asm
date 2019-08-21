afd317a669970294cd5c7b47c18bca915d956b4f
Deepak Gami <35836374+dngami@users.noreply.github.com>
2019-08-21 15:51:26 +0530

    Update timer.asm

--- Assignment_3/timer.asm
+++ Assignment_3/timer.asm
@@ -4,19 +4,19 @@ hof "int8"
 org 9000h                   ; Origin address of program
 
 GTHEX: EQU 030EH    		;Gets hex digits and displays them
-HXDSP: EQU 034FH			;Expands hex digits for display
-OUTPUT:EQU 0389H			;Outputs characters to display
-CLEAR: EQU 02BEH			;Clears the display
-RDKBD: EQU 03BAH			;Reads keyboard
+HXDSP: EQU 034FH		;Expands hex digits for display
+OUTPUT:EQU 0389H		;Outputs characters to display
+CLEAR: EQU 02BEH		;Clears the display
+RDKBD: EQU 03BAH		;Reads keyboard
 KBINT: EQU 3CH		
 
-CURDT: EQU 8FF1H			;address of display
-UPDDT: EQU 044cH			;data of display
-CURAD: EQU 8FEFH			;Updates Data field of the display
-UPDAD: EQU 0440H			;Updates Address field of the display
+CURDT: EQU 8FF1H		;address of display
+UPDDT: EQU 044cH		;data of display
+CURAD: EQU 8FEFH		;Updates Data field of the display
+UPDAD: EQU 0440H		;Updates Address field of the display
 
-MVI A,00H 				  	;next 14 instructions used to display
-MVI B,00H 					  ;"cloc" on display showing start of clock 
+MVI A,00H 			;next 14 instructions used to display
+MVI B,00H 			;"cloc" on display showing start of clock 
 
 LXI H,8840H
 MVI M,0CH
@@ -33,33 +33,33 @@ MVI M,0CH
 LXI H,8840H
 CALL OUTPUT
 CALL RDKBD
-CALL CLEAR 					;clear the display
+CALL CLEAR 			;clear the display
 
 MVI A,00H
 MVI B,00H
-Call gthex 					;take input timer time from keyboard 
-MOV H,D 					  ;store in H-L register
+Call gthex 			;take input timer time from keyboard 
+MOV H,D 			;store in H-L register
 MOV L,E
 
 SHLD CURAD
 JMP MIN
 	
-CHN_HR_MIN:						;start seconds counter from 59s
+CHN_HR_MIN:			;start seconds counter from 59s
 	SHLD CURAD
 	MVI A,59H
-DECR_SEC:                   ; 
-  STA 8200H
-  MVI A,0BH
-  SIM						      ;check after each second if any interrupt is
-	EI						      ;coming
+DECR_SEC:                   
+	STA 8200H
+	MVI A,0BH
+	SIM			;check after each second if any interrupt is
+	EI			;coming
 	LDA 8200H
-	STA CURDT			     	;update display
+	STA CURDT		;update display
 	CALL UPDAD
 	CALL UPDDT
-	CALL DELAY				  ;call delay which runs for 1second
+	CALL DELAY		;call delay which runs for 1second
     
 	LDA CURDT
-	CPI 00H 				    ;check if seconds reach 0
+	CPI 00H 		;check if seconds reach 0
 	JZ MIN                  ;jump to min if seconds reach 0
 	CALL SUBTRACT_FUNCTION 	;bcd subtractor of seconds by 1
 	JMP DECR_SEC
@@ -67,7 +67,7 @@ DECR_SEC:                   ;
 MIN:
 	LHLD CURAD
 	MOV A,L
-	CPI 00H 				;check if minute reach 0
+	CPI 00H 		;check if minute reach 0
 	JZ HOUR					
 	CALL SUBTRACT_FUNCTION 	;if not 0,reduce minute by 1;
 	MOV L,A
@@ -75,8 +75,8 @@ MIN:
 HOUR:					
 	MVI L,59H				
 	MOV A,H
-	CPI 00H					;check if hour reach 0  				
-	JZ BREAK 				;stop timer if hour reaches 0
+	CPI 00H			;check if hour reach 0  				
+	JZ BREAK 		;stop timer if hour reaches 0
 	CALL SUBTRACT_FUNCTION
 	MOV H,A
 	JMP CHN_HR_MIN
@@ -85,12 +85,12 @@ BREAK:
 	RST 5
 	
 	
-DELAY:						;delays machine by 1 second
+DELAY:				;delays machine by 1 second
 	MVI C,03H
 OUTLOOP:
 	LXI D,0A604H			
-INLOOP:						;reapeatedly run inloop as many times as
-	DCX D 					;frequency of microprocessor
+INLOOP:				;reapeatedly run inloop as many times as
+	DCX D 			;frequency of microprocessor
 	MOV A,D
 	ORA E
 	JNZ INLOOP
@@ -102,14 +102,14 @@ INLOOP:						;reapeatedly run inloop as many times as
 ; which then process memory location 8FBF 
 ; store memory location of INTRR label at 8FBF, so that it could process ISR
 
-INTRR:  					 ;ISR to store values of program
-	PUSH PSW         ;Push current program counter in stack
-	CALL RDKBD 			 ;when another interrupt comes,pop original PC
+INTRR:  			;ISR to store values of program
+	PUSH PSW           	;Push current program counter in stack
+	CALL RDKBD 		;when another interrupt comes,pop original PC
 	POP PSW					
-	EI					   	 ;Enabling Inerrupt again
+	EI			;Enabling Inerrupt again
 	RET
 
-SUBTRACT_FUNCTION:	;bcd subtractor 
+SUBTRACT_FUNCTION:		;bcd subtractor 
  STA 8202H
  ANI 0FH
  CPI 00H
